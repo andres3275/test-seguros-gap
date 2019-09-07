19c9c74d98b549351943956792ff201957052d2b
andres <sacastano@indracompany.com>
2019-09-07 11:44:03 -0500

    Ajuste a modelo de base de datos, añadida tabla tipo usuario

--- sql_scripts/3_seed_data.sql
+++ sql_scripts/3_seed_data.sql
@@ -32,14 +32,26 @@ INSERT INTO [dbo].[TipoCubrimiento]
 		   (4,'Perdida','cubrimiento por Perdida')
 GO
 
+INSERT INTO [dbo].[TipoUsuario]
+           ([id]
+           ,[nombre]
+           ,[descripcion])
+     VALUES
+           (1,'Administrador','Administrador del sistema'),
+		   (2,'Asesor','Asesor de la Aseguradora'),
+		   (3,'Cliente','Cliente de la aseguradora')
+GO
+
 INSERT INTO [dbo].[Usuario]
            ([nombre]
            ,[cedula]
            ,[nombreUsuario]
-           ,[contrasenia])
+           ,[contrasenia]
+		   ,[tipoUsuarioId])
      VALUES
-           ('Alexander Vargas' ,'1056699885','',''),
-		   ('Daniela Morales' ,'1056658885','','')
+           ('Alexander Vargas' ,'1056699885','','',2),
+		   ('Daniela Morales' ,'1079658885','','',3),
+		   ('Alejandro Botero' ,'1036654485','','',3)
 GO
 
 
